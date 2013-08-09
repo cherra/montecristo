74ab23cf3f794e2734feadcc5d2f4b666240d68e
cherra <jorge@matica.mx>
2013-08-09 11:30:32 -0500

    Listado de ordenes de salida enviadas

--- application/controllers/almacenes/salidas.php
+++ application/controllers/almacenes/salidas.php
@@ -64,7 +64,15 @@ class Salidas extends CI_Controller {
             //$ruta = $this->r->get_by_id($d->id_ruta)->row();
             $piezas = $this->os->get_piezas($d->id);
             $this->table->add_row(
-                    '<input type="checkbox" name="salidas[]" value="' . $d->id . '"/>', $d->id, $d->fecha, $cliente->nombre, $sucursal->numero . ' ' . $sucursal->nombre, $sucursal->poblacion . ', ' . $sucursal->municipio, !empty($almacen->nombre) ? $almacen->nombre : '', $d->fecha_programada, array('data' => number_format($piezas, 2), 'style' => 'text-align: right;'), $d->origen, array('data' => ($d->estado > 0 && $d->estado < 4 ? anchor($this->folder . $this->clase . 'ordenes_salida_editar/' . $d->id, '<i class="icon-edit"></i>', array('class' => 'btn btn-small', 'title' => 'Editar')) : '<a class="btn btn-small" disabled><i class="icon-edit"></i></a>'), 'style' => 'text-align: right;')
+                    '<i class="'.$this->iconos_estado[$d->estado].'"></i>', 
+                    $d->id, 
+                    $d->fecha, 
+                    $cliente->nombre, $sucursal->numero . ' ' . $sucursal->nombre, $sucursal->poblacion . ', ' . $sucursal->municipio, 
+                    !empty($almacen->nombre) ? $almacen->nombre : '', 
+                    $d->fecha_programada, 
+                    array('data' => number_format($piezas, 2), 'style' => 'text-align: right;'), 
+                    $d->origen, 
+                    array('data' => ($d->estado > 0 && $d->estado < 4 ? anchor($this->folder . $this->clase . 'ordenes_salida_editar/' . $d->id, '<i class="icon-edit"></i>', array('class' => 'btn btn-small', 'title' => 'Editar')) : '<a class="btn btn-small" disabled><i class="icon-edit"></i></a>'), 'style' => 'text-align: right;')
             );
             if ($d->estado == 0)
                 $this->table->add_row_class('muted');
@@ -114,7 +122,11 @@ class Salidas extends CI_Controller {
         $this->table->set_heading('Ruta', 'Ordenes', 'Piezas', 'Peso', '');
         foreach ($datos as $d) {
             $this->table->add_row(
-                    $d->ruta, $d->ordenes, array('data' => number_format($d->piezas, 2), 'style' => 'text-align: right;'), array('data' => number_format($d->peso, 2), 'style' => 'text-align: right;'), array('data' => anchor($this->folder . $this->clase . 'ordenes_salida_ruta/' . $d->id_ruta, '<i class="icon-list"></i>', array('class' => 'btn btn-small', 'title' => 'Ordenes de salida')), 'style' => 'text-align: right;')
+                    $d->ruta, 
+                    $d->ordenes, 
+                    array('data' => number_format($d->piezas, 2), 'style' => 'text-align: right;'), 
+                    array('data' => number_format($d->peso, 2), 'style' => 'text-align: right;'), 
+                    array('data' => anchor($this->folder . $this->clase . 'ordenes_salida_ruta/' . $d->id_ruta, '<i class="icon-list"></i>', array('class' => 'btn btn-small', 'title' => 'Ordenes de salida')), 'style' => 'text-align: right;')
             );
         }
         $data['table'] = $this->table->generate();
@@ -181,7 +193,15 @@ class Salidas extends CI_Controller {
             //$ruta = $this->r->get_by_id($d->id_ruta)->row();
             $piezas = $this->os->get_piezas($d->id);
             $this->table->add_row(
-                    '<input type="checkbox" name="salidas[]" value="' . $d->id . '"/>', $d->id, $d->fecha, $cliente->nombre, $sucursal->numero . ' ' . $sucursal->nombre, $sucursal->poblacion . ', ' . $sucursal->municipio, !empty($almacen->nombre) ? $almacen->nombre : '', $d->fecha_programada, array('data' => number_format($piezas, 2), 'style' => 'text-align: right;'), $d->origen, array('data' => ($d->estado > 0 && $d->estado < 4 ? anchor($this->folder . $this->clase . 'ordenes_salida_editar/' . $d->id, '<i class="icon-edit"></i>', array('class' => 'btn btn-small', 'title' => 'Editar')) : '<a class="btn btn-small" disabled><i class="icon-edit"></i></a>'), 'style' => 'text-align: right;')
+                    '<input type="checkbox" name="salidas[]" value="' . $d->id . '"/>', 
+                    $d->id, 
+                    $d->fecha, 
+                    $cliente->nombre, 
+                    $sucursal->numero . ' ' . $sucursal->nombre, $sucursal->poblacion . ', ' . $sucursal->municipio, 
+                    !empty($almacen->nombre) ? $almacen->nombre : '', $d->fecha_programada,
+                    array('data' => number_format($piezas, 2), 'style' => 'text-align: right;'), 
+                    $d->origen, 
+                    array('data' => ($d->estado > 0 && $d->estado < 4 ? anchor($this->folder . $this->clase . 'ordenes_salida_editar/' . $d->id, '<i class="icon-edit"></i>', array('class' => 'btn btn-small', 'title' => 'Editar')) : '<a class="btn btn-small" disabled><i class="icon-edit"></i></a>'), 'style' => 'text-align: right;')
             );
             if ($d->estado == 0)
                 $this->table->add_row_class('muted');
@@ -230,13 +250,12 @@ class Salidas extends CI_Controller {
         $this->table->set_template($tmpl);
         $this->table->set_heading('Ruta', 'Ordenes', 'Piezas', 'Peso', '');
         foreach ($datos as $d) {
-            //$almacen = $this->a->get_by_id($d->id_almacen)->row();
-            //$sucursal = $this->s->get_by_id($d->id_cliente_sucursal)->row();
-            //$cliente = $this->c->get_by_id($sucursal->id_cliente)->row();
-            //$ruta = $this->r->get_by_id($d->id_ruta)->row();
-            //$piezas = $this->os->get_piezas($d->id);
             $this->table->add_row(
-                    $d->ruta, $d->ordenes, array('data' => number_format($d->piezas, 2), 'style' => 'text-align: right;'), array('data' => number_format($d->peso, 2), 'style' => 'text-align: right;'), array('data' => anchor($this->folder . $this->clase . 'ordenes_salida_ruta_procesadas/' . $d->id_ruta, '<i class="icon-list"></i>', array('class' => 'btn btn-small', 'title' => 'Ordenes de salida')), 'style' => 'text-align: right;')
+                    $d->ruta, 
+                    $d->ordenes, 
+                    array('data' => number_format($d->piezas, 2), 'style' => 'text-align: right;'), 
+                    array('data' => number_format($d->peso, 2), 'style' => 'text-align: right;'),
+                    array('data' => anchor($this->folder . $this->clase . 'ordenes_salida_ruta_procesadas/' . $d->id_ruta, '<i class="icon-list"></i>', array('class' => 'btn btn-small', 'title' => 'Ordenes de salida')), 'style' => 'text-align: right;')
             );
         }
         $data['table'] = $this->table->generate();
@@ -285,6 +304,148 @@ class Salidas extends CI_Controller {
         $this->load->library('pagination');
         $config['base_url'] = site_url($this->folder . $this->clase . 'ordenes_salida_ruta_procesadas');
         $config['total_rows'] = $this->os->count_by_ruta($id_ruta, array('2'), $filtro);
+        $config['per_page'] = $page_limit;
+        $config['uri_segment'] = 4;
+        $this->pagination->initialize($config);
+        $data['pagination'] = $this->pagination->create_links();
+
+        // generar tabla
+        $this->load->library('table');
+        $this->table->set_empty('&nbsp;');
+        $tmpl = array('table_open' => '<table class="' . $this->config->item('tabla_css') . '" >');
+        $this->table->set_template($tmpl);
+        $this->table->set_heading('E', 'Número', 'Fecha', 'Cliente', 'Sucursal', 'Ubicación', 'Almacén', 'Fecha programada', 'Piezas', 'Origen', '');
+        foreach ($datos as $d) {
+            $almacen = $this->a->get_by_id($d->id_almacen)->row();
+            $sucursal = $this->s->get_by_id($d->id_cliente_sucursal)->row();
+            $cliente = $this->c->get_by_id($sucursal->id_cliente)->row();
+            //$ruta = $this->r->get_by_id($d->id_ruta)->row();
+            $piezas = $this->os->get_piezas($d->id);
+            $this->table->add_row(
+                    '<input type="checkbox" name="salidas[]" value="' . $d->id . '"/>', 
+                    $d->id, $d->fecha, 
+                    $cliente->nombre, 
+                    $sucursal->numero . ' ' . $sucursal->nombre, $sucursal->poblacion . ', ' . $sucursal->municipio, 
+                    !empty($almacen->nombre) ? $almacen->nombre : '', $d->fecha_programada, 
+                    array('data' => number_format($piezas, 2), 'style' => 'text-align: right;'), 
+                    $d->origen, 
+                    array('data' => ($d->estado > 0 && $d->estado < 4 ? anchor($this->folder . $this->clase . 'ordenes_salida_editar/' . $d->id, '<i class="icon-edit"></i>', array('class' => 'btn btn-small', 'title' => 'Editar')) : '<a class="btn btn-small" disabled><i class="icon-edit"></i></a>'), 'style' => 'text-align: right;')
+            );
+            if ($d->estado == 0)
+                $this->table->add_row_class('muted');
+            else
+                $this->table->add_row_class('');
+        }
+        $data['table'] = $this->table->generate();
+
+        $this->load->view('almacenes/salidas/proceso_ruta_lista', $data);
+    }
+    
+    public function ordenes_salida_enviadas($offset = 0) {
+        $this->load->model('orden_salida', 'os');
+        $this->load->model('cliente', 'c');
+        $this->load->model('sucursal', 's');
+        $this->load->model('almacen', 'a');
+        $this->load->model('ruta', 'r');
+
+        $this->config->load("pagination");
+
+        $data['titulo'] = 'Ordenes de salida <small>Lista</small>';
+        //$data['link_add'] = anchor($this->folder.$this->clase.'pedidos_agregar','<i class="icon-plus icon-white"></i> Nuevo', array('class' => 'btn btn-inverse'));
+        $data['action'] = $this->folder . $this->clase . 'ordenes_salida_enviadas';
+
+        // Filtro de busqueda (se almacenan en la sesión a través de un hook)
+        $filtro = $this->session->userdata('filtro');
+        if ($filtro)
+            $data['filtro'] = $filtro;
+
+        $page_limit = $this->config->item("per_page");
+        $datos = $this->os->get_by_estado(array('4'), $page_limit, $offset, $filtro)->result();
+
+        // generar paginacion
+        $this->load->library('pagination');
+        $config['base_url'] = site_url($this->folder . $this->clase . 'ordenes_salida_enviadas');
+        $config['total_rows'] = $this->os->count_by_estado(array('4'), $filtro);
+        $config['per_page'] = $page_limit;
+        $config['uri_segment'] = 4;
+        $this->pagination->initialize($config);
+        $data['pagination'] = $this->pagination->create_links();
+
+        // generar tabla
+        $this->load->library('table');
+        $this->table->set_empty('&nbsp;');
+        $tmpl = array('table_open' => '<table class="' . $this->config->item('tabla_css') . '" >');
+        $this->table->set_template($tmpl);
+        $this->table->set_heading('E', 'Número', 'Fecha', 'Cliente', 'Sucursal', 'Ubicación', 'Almacén', 'Fecha programada', 'Piezas', 'Origen', '');
+        foreach ($datos as $d) {
+            $almacen = $this->a->get_by_id($d->id_almacen)->row();
+            $sucursal = $this->s->get_by_id($d->id_cliente_sucursal)->row();
+            $cliente = $this->c->get_by_id($sucursal->id_cliente)->row();
+            //$ruta = $this->r->get_by_id($d->id_ruta)->row();
+            $piezas = $this->os->get_piezas($d->id);
+            $this->table->add_row(
+                    '<i class="'.$this->iconos_estado[$d->estado].'"></i>', 
+                    $d->id, 
+                    $d->fecha, 
+                    $cliente->nombre, 
+                    $sucursal->numero . ' ' . $sucursal->nombre, $sucursal->poblacion . ', ' . $sucursal->municipio, 
+                    !empty($almacen->nombre) ? $almacen->nombre : '', 
+                    $d->fecha_programada, 
+                    array('data' => number_format($piezas, 2), 'style' => 'text-align: right;'), 
+                    $d->origen, 
+                    array('data' => ($d->estado > 0 && $d->estado < 4 ? anchor($this->folder . $this->clase . 'ordenes_salida_editar/' . $d->id, '<i class="icon-edit"></i>', array('class' => 'btn btn-small', 'title' => 'Editar')) : '<a class="btn btn-small" disabled><i class="icon-edit"></i></a>'), 'style' => 'text-align: right;')
+            );
+            if ($d->estado == 0)
+                $this->table->add_row_class('muted');
+            else
+                $this->table->add_row_class('');
+        }
+        $data['table'] = $this->table->generate();
+
+        $this->load->view('lista', $data);
+    }
+
+    public function ordenes_salida_enviadas_ruta($id_ruta = NULL, $offset = 0) {
+        $this->load->model('orden_salida', 'os');
+        $this->load->model('cliente', 'c');
+        $this->load->model('sucursal', 's');
+        $this->load->model('almacen', 'a');
+        $this->load->model('ruta', 'r');
+        $this->load->model('pedido', 'p');
+
+        $this->config->load("pagination");
+
+        $data['titulo'] = 'Ordenes de salida enviadas <small>Lista</small>';
+        //$data['link_add'] = anchor($this->folder.$this->clase.'pedidos_agregar','<i class="icon-plus icon-white"></i> Nuevo', array('class' => 'btn btn-inverse'));
+        $data['link_back'] = anchor($this->folder . $this->clase . 'ordenes_salida_enviadas', '<i class="icon-arrow-left"></i> Regresar', array('class' => 'btn'));
+        $data['action'] = $this->folder . $this->clase . 'ordenes_salida_enviadas_ruta/' . $id_ruta;
+
+        // Filtro de busqueda (se almacenan en la sesión a través de un hook)
+        $filtro = $this->session->userdata('filtro');
+        if ($filtro)
+            $data['filtro'] = $filtro;
+
+        $data['rutas'] = $this->r->get_all()->result();
+        $data['ruta'] = $this->r->get_by_id($id_ruta)->row();
+
+        $data['almacenes'] = $this->a->get_all()->result();
+
+        // Se marca(n) como enviadas las ordenes de salida
+        if ($this->input->post()) {
+            $salidas = $this->input->post('salidas');
+            $id_almacen = $this->input->post('id_almacen');
+            foreach ($salidas as $s) {
+                $this->os->update($s, array('estado' => '2', 'id_almacen' => $id_almacen));
+                $this->p->update_by_orden_salida($s, array('estado' => '3'));
+            }
+        }
+
+        $page_limit = $this->config->item("per_page");
+        $datos = $this->os->get_by_ruta($id_ruta, array('1'), $page_limit, $offset, $filtro)->result();
+        // generar paginacion
+        $this->load->library('pagination');
+        $config['base_url'] = site_url($this->folder . $this->clase . 'ordenes_salida_ruta');
+        $config['total_rows'] = $this->os->count_by_ruta($id_ruta, array('1'), $filtro);
         $config['per_page'] = $page_limit;
         $config['uri_segment'] = 4;
         $this->pagination->initialize($config);
--- application/models/orden_salida.php
+++ application/models/orden_salida.php
@@ -191,6 +191,88 @@ class Orden_salida extends CI_Model {
         return $this->db->get($this->tbl.' os', $limit, $offset);
     }
     
+    function count_by_estado( $estado = array('1'), $filtro = NULL){
+        $this->db->join('OrdenSalidaPresentacion osp','os.id = osp.id_orden_salida');
+        $this->db->join('ProductoPresentaciones ppr','osp.id_producto_presentacion = ppr.id');
+        $this->db->join('ClienteSucursales cs','os.id_cliente_sucursal = cs.id');
+        $this->db->join('Clientes c','cs.id_cliente = c.id');
+        $this->db->join('Usuarios u','os.id_usuario = u.id_usuario');
+        $this->db->join('Rutas r','os.id_ruta = r.id');
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->where("(
+                    os.id LIKE '%".$f."%' OR
+                    c.nombre LIKE '%".$f."%' OR
+                    cs.nombre LIKE '%".$f."%' OR
+                    cs.numero LIKE '%".$f."%' OR
+                    cs.municipio LIKE '%".$f."%' OR
+                    cs.estado LIKE '%".$f."%' OR
+                    u.nombre LIKE '%".$f."%' OR
+                    os.fecha_programada LIKE '%".$f."%' OR
+                    os.fecha_entrega LIKE '%".$f."%' OR
+                    os.fecha LIKE '%".$f."%'
+                    )"
+                );
+            }
+        }
+        $estados = "(";
+        $i = 0;
+        foreach($estado as $e){
+            if($i > 0)
+                $estados .= ' OR ';
+            $estados .= 'os.estado = '.$e;
+            $i++;
+        }
+        $estados .= ")";
+        $this->db->where($estados);
+        $this->db->group_by('os.id');
+        $this->db->order_by('c.nombre, os.id','desc');
+        $query = $this->db->get($this->tbl.' os');
+        return $query->num_rows();
+    }
+    
+    function get_by_estado( $estado = array('1'), $limit = NULL, $offset = 0, $filtro = NULL){
+        $this->db->select('os.*, SUM(osp.cantidad * ppr.peso) AS peso, SUM(osp.cantidad) AS piezas');
+        $this->db->join('OrdenSalidaPresentacion osp','os.id = osp.id_orden_salida');
+        $this->db->join('ProductoPresentaciones ppr','osp.id_producto_presentacion = ppr.id');
+        $this->db->join('ClienteSucursales cs','os.id_cliente_sucursal = cs.id');
+        $this->db->join('Clientes c','cs.id_cliente = c.id');
+        $this->db->join('Usuarios u','os.id_usuario = u.id_usuario');
+        $this->db->join('Rutas r','os.id_ruta = r.id');
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->where("(
+                    os.id LIKE '%".$f."%' OR
+                    c.nombre LIKE '%".$f."%' OR
+                    cs.nombre LIKE '%".$f."%' OR
+                    cs.numero LIKE '%".$f."%' OR
+                    cs.municipio LIKE '%".$f."%' OR
+                    cs.estado LIKE '%".$f."%' OR
+                    u.nombre LIKE '%".$f."%' OR
+                    os.fecha_programada LIKE '%".$f."%' OR
+                    os.fecha_entrega LIKE '%".$f."%' OR
+                    os.fecha LIKE '%".$f."%'
+                    )"
+                );
+            }
+        }
+        $estados = "(";
+        $i = 0;
+        foreach($estado as $e){
+            if($i > 0)
+                $estados .= ' OR ';
+            $estados .= 'os.estado = '.$e;
+            $i++;
+        }
+        $estados .= ")";
+        $this->db->where($estados);
+        $this->db->group_by('os.id');
+        $this->db->order_by('c.nombre, os.id','desc');
+        return $this->db->get($this->tbl.' os', $limit, $offset);
+    }
+    
     function get_piezas($id){
         $this->db->select('SUM(osp.cantidad) AS piezas', FALSE);
         $this->db->join($this->tbl_presentacion.' osp','os.id = osp.id_orden_salida');
