4e4c356872941b047b79e938bb76d2ec091738bf
cherra <jorge@matica.mx>
2014-02-19 14:37:37 -0600

    Corrección: Obtener el código correcto en la función get_by_id en el modelo producto_presentacion

--- application/models/producto_presentacion.php
+++ application/models/producto_presentacion.php
@@ -54,8 +54,11 @@ class Producto_presentacion extends CI_Model {
     * Obtener por id
     */
     function get_by_id($id) {
-        $this->db->where('id', $id);
-        return $this->db->get($this->tbl);
+        $this->db->select('pp.id_producto, pp.id_presentacion, pp.sku, pp.peso, CONCAT(p.codigo,pr.codigo) AS codigo', FALSE);
+        $this->db->join('Productos p','pp.id_producto = p.id');
+        $this->db->join('Presentaciones pr','pp.id_presentacion = pr.id');
+        $this->db->where('pp.id', $id);
+        return $this->db->get($this->tbl.' pp');
     }
     
     /*
