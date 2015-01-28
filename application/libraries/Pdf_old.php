ee36b4c3849e5799ca38ea427014c8d7e015025a
cherra <jorge@matica.mx>
2015-01-28 09:47:48 -0600

    Cambiar librería para PDFs

--- application/libraries/Pdf_old.php
+++ application/libraries/Pdf_old.php
@@ -1,32 +1,27 @@
-<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 /**
  * Description of Mpdf
  *
  * @author cherra
  */
-require_once ("mpdf/mpdf.php");
-
-class Pdf extends mPDF {
+class Pdf {
     
     private $CI;
     
     function __construct() {
+        require_once ("mpdf/mpdf.php");
+        
         $this->CI =& get_instance();
     }
     
-    function render( $html, $pagesize = 'Letter', $watermark = null, $pagenum = false ){
+    function render( $html, $pagesize = 'Letter', $watermark = null ){
         $pdf = new mPDF('utf-8', $pagesize);
         //$pdf->bottom-margin = "500";
         if(!empty($watermark)){
             $pdf->SetWatermarkText($watermark);
             $pdf->showWatermarkText = true;
         }
-        if($pagenum){
-            $pdf->pagenumSuffix = '/';
-            $pdf->SetFooter('{PAGENO}{nbpg}');
-        }
         $pdf->WriteHTML($html);
-        //$pdf->WriteHTML("Prueba de texto");
         
         return $pdf->Output('','S');
         //$pdf->Output();
